f934e76f9e0c9e39fb5e5cf152ffa92b96da69b6
StephenLeonardo <stephenleonardo18@gmail.com>
2024-07-27 23:25:14 +0700

    get notifications/me

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -5,3 +5,11 @@ export const createNotification = async (notification: INotification) => {
   await dbConnect();
   return await Notification.create(notification)
 }
+
+// TODO: add pagination
+export const getNotificationByUserId = async (userId: string) => {
+  await dbConnect();
+  return await Notification.find({
+    recipient_id: userId
+  }).lean();
+}
