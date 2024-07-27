5105424de64e37eea7b57b650ef452665d452d17
tim-nnd <65796225+tim-nnd@users.noreply.github.com>
2024-07-27 07:51:47 +0000

    fix prop error

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -14,7 +14,7 @@ export default function SignInPage() {
   );
 }
 
-function ChromeIcon(props) {
+function ChromeIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -33,26 +33,6 @@ function ChromeIcon(props) {
       <line x1="21.17" x2="12" y1="8" y2="8" />
       <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
       <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
-    </svg>
-  );
-}
-
-function XIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
     </svg>
   );
 }
