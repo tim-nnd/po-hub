46c3e53f4b9bc954f91f27b020a0a791cf9f92fe
reynaldichernando <12949382+reynaldichernando@users.noreply.github.com>
2024-07-27 17:39:10 +0700

    Add authentication with google (#19)

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,11 +1,39 @@
+"use client";
+
+import { useAuth } from "@/components/ui/AuthProvider";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { getAuth, GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
+import { useCallback } from "react";
 
 export default function SignInPage() {
+  const { user, setUser } = useAuth();
+  
+  const handleAuthWithGoogle = useCallback(async () => {
+    try {
+      const result = await signInWithPopup(getAuth(), new GoogleAuthProvider());
+      if (!result) return;
+
+      let user: User = await syncUser();
+      setUser(user);
+
+      alert(`Authenticated as ${user.email}`);
+    } catch (error: any) {
+      const errorMessage = error.message;
+      alert(errorMessage);
+    }
+  }, [])
+
+  const syncUser = async () => {
+    const result = (await axios.post(`/api/users/sync`)).data;
+    return result.user;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">POHub</h1>
       <div className="mt-20">
-        <Button variant="primary" className="flex items-center px-4 py-2 border rounded-md">
+        <Button onClick={handleAuthWithGoogle} variant="primary" className="flex items-center px-4 py-2 border rounded-md">
           <ChromeIcon className="w-6 h-6 mr-2" />
           Sign in with Google
         </Button>
---
+++ pages/api/users/sync.ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { customAlphabet } from "nanoid";
+import { User } from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { getAuth } from "firebase-admin/auth";
+import Cookies from 'cookies';
+import { getFirebaseAdminApp } from "@/lib/getFirebaseAdminApp";
+
+const nanoid = customAlphabet('1234567890', 10);
+
+const postSyncUser = async (userId: string, email: string | undefined) => {
+  await dbConnect();
+  const user = await User.findOne({ _id: userId }).lean();
+  if (user) return;
+
+  try {
+    await User.create({
+      _id: userId,
+      username: `user${nanoid()}`,
+      email: email,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  if (method === 'POST') {
+    try {
+      const auth = getAuth(getFirebaseAdminApp());
+      const cookies = new Cookies(req, res);
+      const token = cookies.get('token');
+
+      if (!token) {
+        throw new Error("No Token")
+      }
+
+      const idToken = await auth.verifyIdToken(token);
+
+      if (!idToken) {
+        throw new Error("Invalid Token")
+      }
+
+      await postSyncUser(idToken.uid, idToken.email);
+      res.status(200).json({ success: true, user: idToken })
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ success: false })
+    }
+  } else {
+    res.status(400).json({ success: false })
+  }
+}
+
+export default handler;
