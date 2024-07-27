441512b1d44757edc432ee63f4b793cf99021def
Stephen Leonardo <stephenleonardo18@gmail.com>
2024-07-27 16:47:35 +0700

    fetch products and their seller (#15)

--- pages/api/products/users.ts
+++ pages/api/products/users.ts
@@ -1,5 +1,23 @@
 // pages/api/users/me.js
-// import { getUserById } from '@lib/db'; // Import your database function
+import dbConnect from "@/lib/dbConnect";
+import { IProduct, Product } from "@/model/Product";
+import { IUser, User } from "@/model/User";
+
+
+const getProductsByUserId = async (userId: string) => {
+    await dbConnect();
+    return await Product.find({
+        seller_id: userId,
+    }).lean();
+}
+
+const getUserById = async (userId: string) => {
+    await dbConnect();
+    return await User.findOne({
+        _id: userId
+    }).lean();
+}
+
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'GET') {
@@ -7,33 +25,42 @@ export default async function handler(req: any, res: any) {
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
-  const reqUserId = req.user_id;
-
+  const { user_id } = req.query;
+  
   try {
 
-    // const products = await getProductsByUserId(reqUserId);
-    // MOCK
-    const products = [
-        {
-            "id": "1",
-            "name": "Product 1",
-            "description": "Description of Product 1",
-            "image_url": "http://example.com/image1.jpg",
-            "state": "AVAILABLE",
-            "close_order_date": new Date("2023-12-31T23:59:59Z"),
-            "available_order_date": new Date("2023-12-31T23:59:59Z"),
-            "min_order": 1,
-            "max_order": 100,
-            "seller_id": "123",
-            "product_order_count": 15
-        }
-    ]
+    const products: IProduct[] | null = await getProductsByUserId(user_id);
     if (!products) {
       return res.status(404).json({ message: 'Products not found' });
     }
 
-    return res.status(200).json(products);
+    const productsResponse = products.map(async product => {
+
+        const seller: IUser | null = await getUserById(product.seller_id)
+
+        return {
+          id: product._id,
+          name: product.name,
+          description: product.description,
+          image_url: product.image_url,
+          state: product.state,
+          closed_at: product.closed_at,
+          available_at: product.available_at,
+          min_order: product.min_order,
+          max_order: product.max_order,
+          seller: {
+            id: seller?._id,
+            name: seller?.username,
+          },
+          order_count: -1 // TODO: add order count
+        }
+      });
+
+    return res.status(200).json(productsResponse);
+
   } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' });
+
+    return res.status(500).json({ message: 'Internal error' });
+
   }
 }
