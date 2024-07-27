374dbbdfd1d89896c71703b9ecd0b03b97f1d7d8
Stephen Leonardo <stephenleonardo18@gmail.com>
2024-07-28 00:36:53 +0700

    add order count in order list (#34)

--- pages/api/orders/me.ts
+++ pages/api/orders/me.ts
@@ -1,14 +1,8 @@
 import dbConnect from "@/lib/dbConnect";
+import { getOrderCountByProductId, getOrdersByUserId } from "@/lib/orders";
+import { getUserUidFromCookies } from "@/lib/users";
 import { IOrder, Order } from "@/model/Order";
 import GetOrderMeResponse, { OrderProductDetail, OrderProductDetailVariation } from "@/model/spec/GetOrderMeResponse";
-
-
-const getOrdersByUserId = async (userId: string, page: number, limit: number) => {
-    await dbConnect();
-    return await Order.find({
-        buyer_id: userId,
-    }).skip((page - 1) * limit).limit(limit).lean();
-}
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'GET') {
@@ -16,19 +10,26 @@ export default async function handler(req: any, res: any) {
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
-  // TODO: get user_id from cookies
-  const user_id = "test";
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 20;
-
+  let userId
+  try {
+    userId = await getUserUidFromCookies(req, res);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal error' });
+  }
   try {
 
-    const orders: IOrder[] | null = await getOrdersByUserId(user_id, page, limit);
+    const orders: IOrder[] | null = await getOrdersByUserId(userId);
     if (!orders) {
       return res.status(404).json({ message: 'Orders not found' });
     }
     
-    const resp = orders.map(order => {
+    const resp = orders.map(async order => {
+
+      const [orderCount] = await Promise.all([
+        getOrderCountByProductId(order.product_detail.product_id)
+      ]);
+
         return {
           id: order._id,
           buyer_id: order.buyer_id,
@@ -44,6 +45,7 @@ export default async function handler(req: any, res: any) {
             available_at: order.product_detail.available_at,
             min_order: order.product_detail.min_order,
             max_order: order.product_detail.max_order,
+            order_count: orderCount,
             seller_id: order.product_detail.seller_id,
             variations: order.product_detail.variations.map(variation => {
               return {
