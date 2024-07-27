fe4888a89116d3783ec9e7c2a86347f59a527845
tim-nnd <timothy.ananda@gmail.com>
2024-07-28 00:51:55 +0700

    update productDetail

--- app/product/detail/page.tsx
+++ app/product/detail/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/Button";
 
 export default function Detail() {
   const orders = 15, minimum = 4, maximum = 20; // TODO: change from response
+  const price = 15000; // TODO: change from response
   var text = ""
   const [orderAmount, setOrderAmount] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    updatePrice()
+  }, [orderAmount]);
+
+  const updatePrice = () => {
+    setTotalPrice(price * orderAmount)
+  }
 
   const stepUp = () => {
     if (orderAmount + orders < maximum) {
@@ -19,6 +29,10 @@ export default function Detail() {
       setOrderAmount(orderAmount - 1);
     }
   }
+
+  const formatIDR = (amount: number) => {
+    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
+  };
 
   if (orders < minimum) {
     text = `has been ordered, needs ${minimum-orders} more to proceed`
@@ -38,10 +52,14 @@ export default function Detail() {
           <div style={{ width: `${((orders+orderAmount)/maximum)*100}%` }} className="absolute top-0 left-0 h-full bg-blue-300 transition-all duration-300"></div>
           <div style={{ width: `${(orders/maximum)*100}%` }} className="absolute top-0 left-0 h-full bg-blue-500 transition-all duration-300"></div>
         </div>
+        <h3 className="text-2xl mt-1">Price: <span className="font-bold">{formatIDR(price)}</span></h3>
         <div className="flex items-center mb-4">
-          <button className="py-2 px-4 text-white bg-blue-900 rounded-l-lg hover:bg-blue-700" onClick={stepDown}>-</button>
-          <input type="number" id="donationAmount" className="text-center w-16 py-2 border-t border-b border-gray-300" value={orderAmount}/>
-          <button className="py-2 px-4 text-white bg-blue-900 rounded-r-lg hover:bg-blue-700" onClick={stepUp}>+</button>
+          <p>{formatIDR(totalPrice)}</p>
+          <div className='ml-auto'>
+            <button className="py-2 px-4 text-white bg-blue-900 rounded-l-lg hover:bg-blue-700" onClick={stepDown}>-</button>
+            <input type="number" id="donationAmount" className="text-center w-16 py-2 border-t border-b border-gray-300" value={orderAmount}/>
+            <button className="py-2 px-4 text-white bg-blue-900 rounded-r-lg hover:bg-blue-700" onClick={stepUp}>+</button>
+          </div>
         </div>
         <Button variant="primary" className="py-3">
           Order
