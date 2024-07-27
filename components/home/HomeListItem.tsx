4f8a3f83520c7b69db5e6cfe3bf530e15f7c6692
reynaldichernando <12949382+reynaldichernando@users.noreply.github.com>
2024-07-28 01:20:59 +0700

    Init api wiring product detail (#38)

--- components/home/HomeListItem.tsx
+++ components/home/HomeListItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import classNames from 'classnames';
+import Link from 'next/link';
 
 interface HomeListItemProps {
+  id: string;
   title: string;
   subtitle: string;
   progress: number;
@@ -9,36 +11,38 @@ interface HomeListItemProps {
   className?: string;
 };
 
-export const HomeListItem: React.FC<HomeListItemProps> = ({ title = 'Title', subtitle = 'lorem ipsum dolor sit amet', progress = '0', image, className, ...props }) => {
+export const HomeListItem: React.FC<HomeListItemProps> = ({ id = '', title = 'Title', subtitle = 'lorem ipsum dolor sit amet', progress = '0', image, className, ...props }) => {
   return (
-    <div 
-      className={classNames('my-4', className)}
-      {...props}
-    >
-      <div className="flex items-start">
-        <img
-          src={image}
-          alt="Image"
-          className='w-28 h-28'
-          style={{ objectFit: 'cover' }}
-        />
-        <div className="flex-1 ml-3">
-          <h1 className="font-bold text-xl">{title}</h1>
-          <p className="mb-2 text-m">{subtitle}</p>
-          <div className="relative pt-1">
-            <div className="flex mb-2 items-center justify-between">
-              <div>
-                <span className="text-xs font-semibold inline-block py-1 px-2 grid place-items-center bg-blue-200 text-blue-600 rounded-full">
-                  {progress}%
-                </span>
+    <Link href="/product/[id]" as={`/product/${id}`}>
+      <div
+        className={classNames('my-4', className)}
+        {...props}
+      >
+        <div className="flex items-start">
+          <img
+            src={image}
+            alt="Image"
+            className='w-28 h-28'
+            style={{ objectFit: 'cover' }}
+          />
+          <div className="flex-1 ml-3">
+            <h1 className="font-bold text-xl">{title}</h1>
+            <p className="mb-2 text-m">{subtitle}</p>
+            <div className="relative pt-1">
+              <div className="flex mb-2 items-center justify-between">
+                <div>
+                  <span className="text-xs font-semibold inline-block py-1 px-2 grid place-items-center bg-blue-200 text-blue-600 rounded-full">
+                    {progress}%
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
-              <div style={{ width: `${progress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+                <div style={{ width: `${progress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
