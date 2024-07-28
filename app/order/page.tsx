e8ac49e98da921ef2425c3ad25cc7eadd6822010
tim-nnd <65796225+tim-nnd@users.noreply.github.com>
2024-07-28 07:33:04 +0700

    fix alert (#67)

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <main className="p-4">
-      <h1 className="text-4xl font-bold mb-2">My Orders</h1>
+      <h1 className="text-4xl font-semibold mb-2">My Orders</h1>
       <hr className="mb-6" />
       {!!loading ? (
         <SpinnerIcon />
